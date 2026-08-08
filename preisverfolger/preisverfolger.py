17943b553db77484d0d27121e3b5ff3b70024067
monkmonk1988 <monkarndt@googlemail.com>
2026-08-08 13:51:20 +0200

    Duplikate bereinigt, Fix in richtiger Datei

--- preisverfolger/preisverfolger.py
+++ preisverfolger/preisverfolger.py
@@ -12,7 +12,7 @@ prices = soup.find_all("p", {"class": "price_color"})
 
 with open("preise.csv", "w",newline="", encoding="utf-8") as datei:
     writer = csv.writer(datei)
-    writer.writerow(["Titel", "Pris"])
+    writer.writerow(["Titel", "Preis"])
 
     for titel, preis in zip(all_soups, prices):
         print(titel.get_text(), "-", preis.get_text())
